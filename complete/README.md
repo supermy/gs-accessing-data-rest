c56251c988036124f2614e2ceed5d5889dbacbbd
supermy <springclick@gmail.com>
2015-01-07 16:44:12 +0800

    配置msqyl运行环境

---
+++ complete/README.md
@@ -0,0 +1,7 @@
+MobileApp
+=========
+
+Test Spring MVC REST application based on mysql
+
+测试 ok
+    mvn spring-boot:run
--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 
 @Configuration
 @EnableJpaRepositories
-@Import(RepositoryRestMvcConfiguration.class)
+@Import({RepositoryRestMvcConfiguration.class,DataBaseConfig.class})
+@PropertySource("classpath:application.properties")
 @EnableAutoConfiguration
 public class Application {
 
